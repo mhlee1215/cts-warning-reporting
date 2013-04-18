bdb096e229b96e869439c4459a7bf4ebdb6007e3
mhlee1215@gmail.com <mhlee1215@gmail.com@40fa1306-632d-68e3-409f-7a4a37707114>
2013-04-18 15:56:09 +0000

    Initial Setting.

---
+++ trunk/CTS_Reporting/src/ac/kaist/cts/dao/UserDaoImpl.java
@@ -0,0 +1,71 @@
+package ac.kaist.cts.dao;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+
+import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
+import org.springframework.stereotype.Repository;
+
+import ac.kaist.cts.domain.User;
+import ac.kaist.cts.domain.UserIdMap;
+
+import com.ibatis.sqlmap.client.SqlMapClient;
+
+@Repository
+public class UserDaoImpl extends SqlMapClientDaoSupport implements UserDao {
+	
+	 @Resource(name="sqlMapClient")
+	 protected void initDAO(SqlMapClient sqlMapClient) {        
+		 this.setSqlMapClient(sqlMapClient);
+	 } 
+	
+	
+	@SuppressWarnings("unchecked")
+	public List<User> findAll() {	
+		List<User> array = getSqlMapClientTemplate().queryForList("UserSql.readUserList");
+		return array;
+	}
+
+
+	public User readUser(User user) {
+		User result = (User)getSqlMapClientTemplate().queryForObject("UserSql.readUser", user);
+		return result;
+	}
+
+
+	public void createUser(User user) {
+		getSqlMapClientTemplate().insert("UserSql.createUser", user);
+	}
+
+
+	public void deleteUser(User user) {
+		getSqlMapClientTemplate().delete("UserSql.deleteUser", user);
+		
+	}
+
+
+	public void updateUser(User user) {
+		getSqlMapClientTemplate().update("UserSql.updateUser", user);
+	}
+
+
+	public void createUserIdMap(UserIdMap userIdMap) {
+		getSqlMapClientTemplate().insert("UserSql.createUserIdMap", userIdMap);
+		
+	}
+
+
+	public int getNextUserIdMap() {
+		int result = (Integer)getSqlMapClientTemplate().queryForObject("UserSql.getNextId");
+		return result;
+	}
+
+
+	public UserIdMap getUserIdMap(UserIdMap userIdMap) {
+		UserIdMap result = (UserIdMap)getSqlMapClientTemplate().queryForObject("UserSql.getUserIdMap", userIdMap);
+		return result;
+	}
+
+}
